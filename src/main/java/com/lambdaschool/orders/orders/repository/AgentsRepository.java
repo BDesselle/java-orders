29b29f3e85b90ffc67f698562c9f733e32dd6a21
BDesselle <desselle.brandon2@gmail.com>
2019-05-09 19:05:46 -0500

    Delete AgentsRepository.java

--- src/main/java/com/lambdaschool/orders/orders/repository/AgentsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lambdaschool.orders.orders.repository;
-
-import com.lambdaschool.orders.orders.models.Agents;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgentsRepository extends JpaRepository<Agents, Long> {
-
-}
